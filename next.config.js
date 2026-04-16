5f638c6446c59e0d51a6d60b2abf7e9fb4d38144
Muhamad Holis <email-anda@example.com>
2026-04-16 14:37:29 +0700

    fix: update tradingEngine import oanda→monex, fix next.config serverActions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  experimental: { serverActions: false },
+  experimental: {},
 };
+
 module.exports = nextConfig;
